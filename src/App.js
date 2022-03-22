e5131dd800aef83bd5e14240d2a118199fcc69d5
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-23 01:51:56 +0500

    Set up redux and implement shuffle message func

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './assets/stylesheets/App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import globalStore from './redux/configureStore';
 import Messages from './components/Messages';
 import Home from './components/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>Wellcome to the messages app</h1>
-        <Routes>
-          <Route path='/' exact element={<Messages />} />
-          <Route path='/home' exact element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={globalStore}>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Wellcome to the messages app</h1>
+          <Routes>
+            <Route path="/" exact element={<Messages />} />
+            <Route path="/home" exact element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getData } from '../redux/reducer/messages';
 
-const Messages = () => (
-  <div className="messages">
-    <h1>Messages will be here</h1>
-  </div>
-);
+const Messages = () => {
+  const dispatch = useDispatch();
+  const globalState = useSelector((state) => state.messagesReducer);
+  const [message, setMessage] = useState('Holla, amigos!');
+
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
+
+  const shuffleMessage = () => {
+    const rand = Math.floor(Math.random() * globalState.length);
+    setMessage(globalState[rand].text);
+  };
+
+  return (
+    <div className="messages">
+      <h1>{message}</h1>
+      <button onClick={shuffleMessage} type="button">Change the language</button>
+    </div>
+  );
+};
 
 export default Messages;
